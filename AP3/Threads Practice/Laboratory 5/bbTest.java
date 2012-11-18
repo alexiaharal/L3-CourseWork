103a5a6babf3f1c4f738083d7e1b22ad34d87710
Mamas Nicolaou <mamasnicolaou@gmail.com>
2012-11-18 14:08:31 +0000

    Laboratory 5 directory

--- AP3/Threads Practice/Laboratory 5/bbTest.java
+++ AP3/Threads Practice/Laboratory 5/bbTest.java
@@ -2,38 +2,43 @@ import java.util.Scanner;
 
 public class bbTest {
 
-   public static class Cons implements Runnable {
-      private BoundedBuffer<String> theBuffer;
+	public static class Cons implements Runnable {
+		private BoundedBuffer<String> theBuffer;
 
-      public Cons(BoundedBuffer<String> theBuffer) {
-         this.theBuffer = theBuffer;
-      }
+		public Cons(BoundedBuffer<String> theBuffer) {
+			this.theBuffer = theBuffer;
+		}
 
-      public void run() {
-         while (true) {
-            try {
-               String s = theBuffer.get();
-	       System.out.println(s);
-	    } catch (InterruptedException e) { return; }
-	 }
-      }
-   }
+		public void run() {
+			while (true) {
+				try {
+					String s = theBuffer.get();
+					System.out.println(s);
+				} catch (InterruptedException e) {
+					return;
+				}
+			}
+		}
+	}
 
-   public static void main(String Args[]) {
-      BoundedBuffer<String> theBuffer = new BoundedBuffer<String>();
-      Cons c = new Cons(theBuffer);
-      Thread t = new Thread(c);
-      Scanner stdin = new Scanner(System.in);
+	/*public static void main(String Args[]) {
+		BoundedBuffer<String> theBuffer = new BoundedBuffer<String>();
+		Cons c = new Cons(theBuffer);
+		Thread t = new Thread(c);
+		System.out.println("Hello");
+		Scanner stdin = new Scanner(System.in);
 
-      t.start();
-      while (stdin.hasNext()) {
-         String s = stdin.nextLine();
-	 theBuffer.put(s);
-      }
-      t.interrupt();
-      try {
-         t.join();
-      } catch (Exception e) {}
-      System.exit(0);
-   }
+		t.start();
+		while (stdin.hasNext()) {
+			String s = stdin.nextLine();
+			theBuffer.put(s);
+		}
+		t.interrupt();
+		try {
+			t.join();
+		} catch (Exception e) {
+		}
+		System.out.println("I am done!");
+		System.exit(0);
+	}*/
 }
