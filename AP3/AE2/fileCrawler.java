13c4415fa2c2bbc7e5f594dcfd1eefd041bc77db
Mamas Nicolaou <mamasnicolaou@gmail.com>
2012-11-19 22:04:53 +0000

    First draft - Not fully functional

--- AP3/AE2/fileCrawler.java
+++ AP3/AE2/fileCrawler.java
@@ -4,73 +4,207 @@ import java.util.*;
 import java.io.*;
 
 public class fileCrawler {
+	// Using LinkedBlockingQueue to make it safe for threads to
+	// queue/dequeue elements concurrently
+	private LinkedBlockingQueue<String> workQueue;
+	private ConcurrentHashMap<String, LinkedList<String>> anotherStructure;
+	private Object lockObject;
 
-/*
- * routine to convert bash pattern to regex pattern
- * 
- * e.g. if bashpat is "*.c", pattern generated is "^.*\.c$"
- *      if bashpat is "a.*", pattern generated is "^a\..*$"
- *
- * i.e. '*' is converted to ".*"
- *      '.' is converted to "\."
- *      '?' is converted to "."
- *      '^' is put at the beginning of the regex pattern
- *      '$' is put at the end of the regex pattern
- *
- * assumes 'pattern' is large enough to hold the regular expression
- */
+	private class Worker implements Runnable {
 
-public static Pattern cvtPattern(String bashpat) {
-	StringBuilder pat = new StringBuilder();
-	int start, length;
-	pat.append('^');
-	if (bashpat.charAt(0) == '\'') { // double quoting on Windows
-		start = 1;
-		length = bashpat.length() - 1;
-	} else {
-		start = 0;
-		length = bashpat.length();
+		private LinkedBlockingQueue<String> queue;
+		private ConcurrentHashMap<String, LinkedList<String>> otherStructure;
+		private Object lockObject;
+
+		public Worker(LinkedBlockingQueue<String> q,
+				ConcurrentHashMap<String, LinkedList<String>> s, Object lock) {
+			queue = q;
+			otherStructure = s;
+			lockObject = lock;
+		}
+//Part of the code in Worker.run was taken from the example on the 
+//AE2 specification document section 7.
+		public void run() {
+			//main checks if "directory" is actually a directory and not a file
+			//before adding it to the WorkQueue
+			String directory;
+			//System.out.println("Internal queue has element: " +queue.poll());
+			while ((directory = queue.poll()) != null) {
+				System.out.println("Queue has something");
+				try{
+					File dir = new File(directory);//create a file object
+					System.out.println("Has set");
+					if(dir.isDirectory())
+						System.out.println("Its directory");
+					System.out.println("Not dir");
+					String files[] = dir.list();
+					for (String file: files){
+						System.out.println(file);}
+				}catch (Exception e){
+					System.err.println("Error processing file");
+				}}
+			}
+		}
+		public fileCrawler(){
+			workQueue = new LinkedBlockingQueue<String>();
+			anotherStructure = new ConcurrentHashMap<String,LinkedList<String>>();
+			lockObject = new Object();
+		}
+
+	public Worker createWorker() {
+		return new Worker(workQueue, anotherStructure, lockObject);
 	}
-	for (int i = start; i < length; i++) {
-		switch(bashpat.charAt(i)) {
-			case '*': pat.append('.'); pat.append('*'); break;
-			case '.': pat.append('\\'); pat.append('.'); break;
-			case '?': pat.append('.'); break;
-			default: pat.append(bashpat.charAt(i)); break;
+	/*
+	 * routine to convert bash pattern to regex pattern
+	 * 
+	 * e.g. if bashpat is "*.c", pattern generated is "^.*\.c$" if bashpat is
+	 * "a.*", pattern generated is "^a\..*$"
+	 * 
+	 * i.e. '*' is converted to ".*" '.' is converted to "\." '?' is converted
+	 * to "." '^' is put at the beginning of the regex pattern '$' is put at the
+	 * end of the regex pattern
+	 * 
+	 * assumes 'pattern' is large enough to hold the regular expression
+	 */
+
+	public static Pattern cvtPattern(String bashpat) {
+		StringBuilder pat = new StringBuilder();
+		int start, length;
+		pat.append('^');
+		if (bashpat.charAt(0) == '\'') { // double quoting on Windows
+			start = 1;
+			length = bashpat.length() - 1;
+		} else {
+			start = 0;
+			length = bashpat.length();
+		}
+		for (int i = start; i < length; i++) {
+			switch (bashpat.charAt(i)) {
+			case '*':
+				pat.append('.');
+				pat.append('*');
+				break;
+			case '.':
+				pat.append('\\');
+				pat.append('.');
+				break;
+			case '?':
+				pat.append('.');
+				break;
+			default:
+				pat.append(bashpat.charAt(i));
+				break;
+			}
+		}
+		pat.append('$');
+		Pattern p = Pattern.compile(new String(pat));
+		return p;
+	}
+
+	private static void matchRegex(Pattern regExpression, String filename) {
+		Matcher m = regExpression.matcher(filename);
+		if (m.matches()) {
+			System.out.println("It matches bitch!");
+		} else {
+			System.out.println("No match");
 		}
 	}
-	pat.append('$');
-	Pattern p=Pattern.compile(new String(pat));
-	return p;
-}
 
-public fileCrawler(){
-	//Using LinkedBlockingQueue to make it safe for threads to 
-	//queue/dequeue elements concurrently
-	LinkedBlockingQueue<String> workQueue = new LinkedBlockingQueue<String>();
+	public static void main(String Arg[]) throws Exception{
+		String directory = "";
+		String pattern = "";
+		fileCrawler crawler = new fileCrawler();
+		if (Arg.length < 2) {
+			System.err.println("Usage: ./fileCrawler pattern [dir] ... ");
+			System.exit(1);
+		}
+		// convert and compile the given pattern to Java Regex Pattern
+		Pattern pat = cvtPattern(Arg[0]);
+		// save given directory name into directory variable
+		directory = Arg[1];
+		crawler.workQueue.put(directory);
+		System.out.println(crawler.workQueue.peek());
+		Thread t = new Thread(crawler.createWorker());
+		t.start();
+		t.join();
 
-}
+		//
+		// obtain the number of threads
+		//
+/*		String thstring = System.getenv("CRAWLER_THREADS");
+		Integer nthreads;
+		if (thstring == null)
+			nthreads = new Integer(2);
+		else
+			nthreads = new Integer(thstring);*/
 
+		//
+		// now place each directory into the workQ
+		//
+	/*	LinkedList<String> objects = new LinkedList<String>();
+		for (int i = fileStart; i < Arg.length; i++) {
+			int ndx = Arg[i].lastIndexOf('.');
+			String root, ext, obj;
+			if (ndx == -1) {
+				root = new String(Arg[i]);
+				ext = new String("");
+			} else {
+				root = Arg[i].substring(0, ndx);
+				ext = Arg[i].substring(ndx + 1);
+			}
+			if (ext.compareTo("c") != 0 && ext.compareTo("y") != 0
+					&& ext.compareTo("l") != 0) {
+				System.err.println("File arguments must end in .c, .y, or .l: "
+						+ Arg[i]);
+				System.exit(1);
+			}
+			obj = root + ".o";
+			objects.add(obj);
+			LinkedList<String> ll = new LinkedList<String>();
+			ll.add(Arg[i]);
+			ic.theTable.put(obj, ll);
+			try {
+				ic.workQ.put(Arg[i]);
+			} catch (Exception e) {
+			}
+			;
+			ll = new LinkedList<String>();
+			ic.theTable.put(Arg[i], ll);
+		}
 
-private static void matchRegex(Pattern regExpression, String filename){
-	Matcher m=regExpression.matcher(filename);
-	if(m.matches()){
-		System.out.println("It matches bitch!");
-	}else{
-		System.out.println("No match");
+		//
+		// now process each file in the workQ
+		//
+		int N = nthreads.intValue();
+		ArrayList<Thread> thread = new ArrayList<Thread>(N);
+		for (int i = 0; i < N; i++) {
+			Thread t = new Thread(ic.createWorker());
+			thread.add(t);
+			t.start();
+		}
+		for (int i = 0; i < N; i++)
+			try {
+				thread.get(i).join();
+			} catch (Exception e) {
+			}
+		;
+		//
+
+		//
+		// now print dependencies on System.out
+		//
+		Iterator<String> it = objects.iterator();
+		while (it.hasNext()) {
+			LinkedList<String> toProcess = new LinkedList<String>();
+			String obj = it.next();
+			System.out.print(obj + ":");
+			HashMap<String, Integer> seen = new HashMap<String, Integer>();
+			seen.put(obj, new Integer(1));
+			toProcess.add(obj);
+			ic.printDependencies(seen, toProcess);
+			System.out.print("\n");
+		}*/
+		System.out.println("This is now terminated");
+		System.exit(0);
 	}
-}
-public static void main(String Arg[]){
-	String directory="";
-	String pattern="";
-	if (Arg.length<2){
-		System.err.println("Usage: ./fileCrawler pattern [dir] ... " );
-		System.exit(1);
-	}
-	//convert the given pattern to Java Regex
-	Pattern pat=cvtPattern(Arg[0]);
-	matchRegex(pat,"cis.java");
-
-	System.exit(0);
-}
 }
