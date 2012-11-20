f0c3d3d28a63c8196005ffc2e53b9a966f5d323e
Mamas Nicolaou <mamasnicolaou@gmail.com>
2012-11-20 02:51:51 +0000

    Thread can receive files and can match filenames correctly.

--- AP3/AE2/DirectoryTree.java
+++ AP3/AE2/DirectoryTree.java
@@ -1,4 +1,5 @@
 import java.io.* ;
+import java.util.* ;
 /**
 * A simple class to print out a directory tree, depth first
 *
@@ -10,27 +11,36 @@ public class DirectoryTree {
 * to be a directory.
 * @param name The name of a directory to visit
 */
-public void processDirectory( String name ) {
+public LinkedList<String> processDirectory( String name ) {
+	LinkedList<String> lista=new LinkedList<String>();
 	try {
 		File file = new File(name); // create a File object
 		if (file.isDirectory()) { // a directory - could be symlink
 			String entries[] = file.list();
 		if (entries != null) { // not a symlink
-			System.out.println(name); // print out name
+			//System.out.println(name); // print out name
+
 			for (String entry : entries ) {
 			if (entry.compareTo(".") == 0)
 				continue;
 			if (entry.compareTo("..") == 0)
 				continue;
-			processDirectory(name+"/"+entry);
+			LinkedList<String> up=processDirectory(name+"/"+entry);
+			while(!up.isEmpty())
+				lista.add(up.poll());
+			}
+			
 			}
 		}
-		}
+
 	} catch (Exception e) {
 		System.err.println("Error processing "+name+": "+e);
 	}
+	System.out.println(lista.size());
+	return lista;
 
 }
+
 /**
 * The program starts here.
 * @param args The arguments from the command line
@@ -38,14 +48,20 @@ public void processDirectory( String name ) {
 public static void main( String args[] ) {
 // Create an object of this class
 	DirectoryTree dt = new DirectoryTree() ;
+	LinkedList<String> listo=new LinkedList<String>();
 	if( args.length == 0 ) {
 	// If there are no arguments, process the current directory
 		dt.processDirectory( "." ) ;
 	} else {
 	// Else process every argument sequentially
 		for( String arg : args ) {
-			dt.processDirectory( arg ) ;
-		}
+			for (String s:dt.processDirectory(arg)){
+				listo.add(s);
+			}
 	}
 	}
+	for (String s:listo){
+		System.out.println(s);
+	}
+}
 }
--- AP3/AE2/fileCrawler.java
+++ AP3/AE2/fileCrawler.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class fileCrawler {
 	// Using LinkedBlockingQueue to make it safe for threads to
 	// queue/dequeue elements concurrently
-	private LinkedBlockingQueue<String> workQueue;
+	private static LinkedBlockingQueue<String> workQueue;
 	private ConcurrentHashMap<String, LinkedList<String>> anotherStructure;
 	private Object lockObject;
 
@@ -15,45 +15,62 @@ public class fileCrawler {
 		private LinkedBlockingQueue<String> queue;
 		private ConcurrentHashMap<String, LinkedList<String>> otherStructure;
 		private Object lockObject;
+		private Pattern pat;
 
 		public Worker(LinkedBlockingQueue<String> q,
-				ConcurrentHashMap<String, LinkedList<String>> s, Object lock) {
+				ConcurrentHashMap<String, LinkedList<String>> s, Object lock,
+				Pattern p) {
 			queue = q;
-			otherStructure = s;
+			otherStructure = new ConcurrentHashMap<String, LinkedList<String>>();
 			lockObject = lock;
+			pat = p;
 		}
-//Part of the code in Worker.run was taken from the example on the 
-//AE2 specification document section 7.
+
+		// Part of the code in Worker.run was taken from the example on the
+		// AE2 specification document section 7.
 		public void run() {
 			//main checks if "directory" is actually a directory and not a file
 			//before adding it to the WorkQueue
 			String directory;
+			LinkedList<String> list=new LinkedList<String>();
 			//System.out.println("Internal queue has element: " +queue.poll());
 			while ((directory = queue.poll()) != null) {
-				System.out.println("Queue has something");
 				try{
 					File dir = new File(directory);//create a file object
-					System.out.println("Has set");
-					if(dir.isDirectory())
-						System.out.println("Its directory");
-					System.out.println("Not dir");
 					String files[] = dir.list();
-					for (String file: files){
-						System.out.println(file);}
-				}catch (Exception e){
+					for (String temp: files){
+						File file=new File(temp);
+						if (file.isDirectory()){
+							continue;
+						}else{
+							if (matchRegex(pat,file.getName())){
+								System.out.println(file.getName());
+								list.add(directory+"/"+file.getName());
+								//otherStructure.put(file.getName(),list);
+								if (otherStructure.get(directory) == null) {
+									otherStructure.put(directory, list);
+							System.out.println("Path to matched:"+otherStructure.get(directory).getFirst());
+
+							}
+						}
+					}
+					}}catch (Exception e){
 					System.err.println("Error processing file");
 				}}
-			}
-		}
-		public fileCrawler(){
-			workQueue = new LinkedBlockingQueue<String>();
-			anotherStructure = new ConcurrentHashMap<String,LinkedList<String>>();
-			lockObject = new Object();
-		}
+		
+			
+			}}
 
-	public Worker createWorker() {
-		return new Worker(workQueue, anotherStructure, lockObject);
+	public fileCrawler() {
+		workQueue = new LinkedBlockingQueue<String>();
+		anotherStructure = new ConcurrentHashMap<String, LinkedList<String>>();
+		lockObject = new Object();
 	}
+
+	public Worker createWorker(Pattern pattern) {
+		return new Worker(workQueue, anotherStructure, lockObject, pattern);
+	}
+
 	/*
 	 * routine to convert bash pattern to regex pattern
 	 * 
@@ -101,16 +118,37 @@ public class fileCrawler {
 		return p;
 	}
 
-	private static void matchRegex(Pattern regExpression, String filename) {
+	private static boolean matchRegex(Pattern regExpression, String filename) {
 		Matcher m = regExpression.matcher(filename);
 		if (m.matches()) {
-			System.out.println("It matches bitch!");
+			return true;
 		} else {
-			System.out.println("No match");
+			return false;
 		}
 	}
 
-	public static void main(String Arg[]) throws Exception{
+public static void processDirectory( String name) {
+	try {
+		File file = new File(name); // create a File object
+		if (file.isDirectory()) { // a directory - could be symlink
+			String entries[] = file.list();
+		if (entries != null) { // not a symlink
+			workQueue.add(name);
+			for (String entry : entries ) {
+			if (entry.compareTo(".") == 0)
+				continue;
+			if (entry.compareTo("..") == 0)
+				continue;
+			processDirectory(name+"/"+entry);
+			}
+		}
+		}
+	} catch (Exception e) {
+		System.err.println("Error processing "+name+": "+e);
+	}
+
+}
+	public static void main(String Arg[]) throws Exception {
 		String directory = "";
 		String pattern = "";
 		fileCrawler crawler = new fileCrawler();
@@ -122,88 +160,52 @@ public class fileCrawler {
 		Pattern pat = cvtPattern(Arg[0]);
 		// save given directory name into directory variable
 		directory = Arg[1];
-		crawler.workQueue.put(directory);
-		System.out.println(crawler.workQueue.peek());
-		Thread t = new Thread(crawler.createWorker());
+		processDirectory(directory);
+		Thread t = new Thread(crawler.createWorker(pat));
 		t.start();
 		t.join();
-
+		//Iterator<String> it=
 		//
 		// obtain the number of threads
 		//
-/*		String thstring = System.getenv("CRAWLER_THREADS");
-		Integer nthreads;
-		if (thstring == null)
-			nthreads = new Integer(2);
-		else
-			nthreads = new Integer(thstring);*/
+		/*
+		 * String thstring = System.getenv("CRAWLER_THREADS"); Integer nthreads;
+		 * if (thstring == null) nthreads = new Integer(2); else nthreads = new
+		 * Integer(thstring);
+		 */
 
 		//
 		// now place each directory into the workQ
 		//
-	/*	LinkedList<String> objects = new LinkedList<String>();
-		for (int i = fileStart; i < Arg.length; i++) {
-			int ndx = Arg[i].lastIndexOf('.');
-			String root, ext, obj;
-			if (ndx == -1) {
-				root = new String(Arg[i]);
-				ext = new String("");
-			} else {
-				root = Arg[i].substring(0, ndx);
-				ext = Arg[i].substring(ndx + 1);
-			}
-			if (ext.compareTo("c") != 0 && ext.compareTo("y") != 0
-					&& ext.compareTo("l") != 0) {
-				System.err.println("File arguments must end in .c, .y, or .l: "
-						+ Arg[i]);
-				System.exit(1);
-			}
-			obj = root + ".o";
-			objects.add(obj);
-			LinkedList<String> ll = new LinkedList<String>();
-			ll.add(Arg[i]);
-			ic.theTable.put(obj, ll);
-			try {
-				ic.workQ.put(Arg[i]);
-			} catch (Exception e) {
-			}
-			;
-			ll = new LinkedList<String>();
-			ic.theTable.put(Arg[i], ll);
-		}
-
-		//
-		// now process each file in the workQ
-		//
-		int N = nthreads.intValue();
-		ArrayList<Thread> thread = new ArrayList<Thread>(N);
-		for (int i = 0; i < N; i++) {
-			Thread t = new Thread(ic.createWorker());
-			thread.add(t);
-			t.start();
-		}
-		for (int i = 0; i < N; i++)
-			try {
-				thread.get(i).join();
-			} catch (Exception e) {
-			}
-		;
-		//
-
-		//
-		// now print dependencies on System.out
-		//
-		Iterator<String> it = objects.iterator();
-		while (it.hasNext()) {
-			LinkedList<String> toProcess = new LinkedList<String>();
-			String obj = it.next();
-			System.out.print(obj + ":");
-			HashMap<String, Integer> seen = new HashMap<String, Integer>();
-			seen.put(obj, new Integer(1));
-			toProcess.add(obj);
-			ic.printDependencies(seen, toProcess);
-			System.out.print("\n");
-		}*/
+		/*
+		 * LinkedList<String> objects = new LinkedList<String>(); for (int i =
+		 * fileStart; i < Arg.length; i++) { int ndx = Arg[i].lastIndexOf('.');
+		 * String root, ext, obj; if (ndx == -1) { root = new String(Arg[i]);
+		 * ext = new String(""); } else { root = Arg[i].substring(0, ndx); ext =
+		 * Arg[i].substring(ndx + 1); } if (ext.compareTo("c") != 0 &&
+		 * ext.compareTo("y") != 0 && ext.compareTo("l") != 0) {
+		 * System.err.println("File arguments must end in .c, .y, or .l: " +
+		 * Arg[i]); System.exit(1); } obj = root + ".o"; objects.add(obj);
+		 * LinkedList<String> ll = new LinkedList<String>(); ll.add(Arg[i]);
+		 * ic.theTable.put(obj, ll); try { ic.workQ.put(Arg[i]); } catch
+		 * (Exception e) { } ; ll = new LinkedList<String>();
+		 * ic.theTable.put(Arg[i], ll); }
+		 * 
+		 * // // now process each file in the workQ // int N =
+		 * nthreads.intValue(); ArrayList<Thread> thread = new
+		 * ArrayList<Thread>(N); for (int i = 0; i < N; i++) { Thread t = new
+		 * Thread(ic.createWorker()); thread.add(t); t.start(); } for (int i =
+		 * 0; i < N; i++) try { thread.get(i).join(); } catch (Exception e) { }
+		 * ; //
+		 * 
+		 * // // now print dependencies on System.out // Iterator<String> it =
+		 * objects.iterator(); while (it.hasNext()) { LinkedList<String>
+		 * toProcess = new LinkedList<String>(); String obj = it.next();
+		 * System.out.print(obj + ":"); HashMap<String, Integer> seen = new
+		 * HashMap<String, Integer>(); seen.put(obj, new Integer(1));
+		 * toProcess.add(obj); ic.printDependencies(seen, toProcess);
+		 * System.out.print("\n"); }
+		 */
 		System.out.println("This is now terminated");
 		System.exit(0);
 	}
