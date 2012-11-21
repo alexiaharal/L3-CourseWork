a1b62ded2d3b4a0fd8ac9816b540aae079f28872
Mamas Nicolaou <mamasnicolaou@gmail.com>
2012-11-20 17:59:14 -0800

    finalised build.sh

--- AP3/AE2/build.sh
+++ AP3/AE2/build.sh
@@ -1,15 +1,2 @@
-JFLAGS = -cp .
-JC = javac
-.SUFFIXES: .java .class
-.java.class:
-        $(JC) $(JFLAGS) $*.java
-
-CLASSES = \
-        fileCrawler.java \
-
-default: classes
-
-classes: $(CLASSES:.java=.class)
-
-clean:
-        $(RM) *.class
+#!/bin/bash          
+javac fileCrawler.java
