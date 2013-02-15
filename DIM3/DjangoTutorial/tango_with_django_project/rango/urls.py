e78723a53aa1f49a9e05f483e9fbc93e1343368b
Mamas Nicolaou <mamasnicolaou@gmail.com>
2013-02-15 15:42:36 +0000

    lala

--- DIM3/DjangoTutorial/tango_with_django_project/rango/urls.py
+++ DIM3/DjangoTutorial/tango_with_django_project/rango/urls.py
@@ -4,5 +4,6 @@ from rango import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^about.html', views.about, name='about')
+        url(r'^about.html', views.about, name='about'),
+        url(r'^cat/(?P<category_name_url>\w+)', views.category, name='category'),
 )
--- DIM3/DjangoTutorial/tango_with_django_project/rango/views.py
+++ DIM3/DjangoTutorial/tango_with_django_project/rango/views.py
@@ -9,6 +9,10 @@ def index(request):
         
         #Request all the categories
         cat_list = Category.objects.all()
+        # add the cat_url data to each category
+        for cat in cat_list:
+			category_name = cat.name
+			cat.url = encode_category(category_name)
         #Put the data into the context
         context = RequestContext(request, {'cat_list':cat_list})
         # render the template using the provided context and return as http response.
@@ -23,3 +27,29 @@ def about(request):
         context = RequestContext(request, {})
         # render the template using the provided context and return as http response.
         return HttpResponse(template.render(context))
+        
+def category(request, category_name_url):
+        template = loader.get_template('rango/category.html')
+
+        category_name = decode_category(category_name_url)
+        context_dict = {'category_name_url': category_name_url,
+                                'category_name': category_name}
+        # Select the Category object given its name.
+        # In models, we defined name to be unique,
+        # so there so only be one, if one exists.
+        cat = Category.objects.filter(name=category_name)
+        if cat:
+                # selects all the pages associated with the selected category
+                pages = Page.objects.filter(category=cat)
+                context_dict['pages'] = pages
+
+        context = RequestContext(request, context_dict)
+        return HttpResponse(template.render(context))
+        
+def encode_category(category_name):
+        # returns the name converted for insert into url
+        return category_name.replace(' ','_')
+
+def decode_category(category_url):
+        # returns the category name given the category url portion
+        return category_url.replace('_',' ')
